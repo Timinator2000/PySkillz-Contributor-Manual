7e24e71a31b3cc6da6e10b37d0db461c9ce0d841
Timinator2000 <v0t2ugh@gmail.com>
2025-09-25 03:20:48 -0600

    adding customization sections

---
+++ python-project/exercises/hello_world_3x_v2/hello_world_3x.py
@@ -0,0 +1,6 @@
+def hello_world_3x() -> None:
+    # Your code goes here.
+    
+    # Write an answer using print
+    # To debug: import sys at the top of this script
+    #           print("Debug messages...", file=sys.stderr, flush=True)
---
+++ python-project/exercises/hello_world_3x_v2/hello_world_3x_solution.py
@@ -0,0 +1,4 @@
+def hello_world_3x() -> None:
+    print('Hello, World!')
+    print('Hello, World!')
+    print('Hello, World!')
