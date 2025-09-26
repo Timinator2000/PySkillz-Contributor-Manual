963a34f8c2b7715ba4742b45670a20bfae2a2f82
Timinator2000 <v0t2ugh@gmail.com>
2025-09-26 08:09:46 -0600

    working on hello world 3 times

--- python-project/exercises/hello_world_3x_restricted_statements/hello_world_3x_restricted_statements.py
+++ python-project/exercises/hello_world_3x_restricted_statements/hello_world_3x_restricted_statements.py
@@ -1,5 +1,5 @@
 def hello_world_3x_restricted_statements() -> None:
-    x = 3 + 6
+    x = 3 + 6 + 7
     print('\n'.join(['Hello, World!', 'Hello, World!', 'Hello, World!']))
 
     # Your code goes here.
