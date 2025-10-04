e2488ac084cfc44bb2d495a02976d25f8830243e
Timinator2000 <v0t2ugh@gmail.com>
2025-10-04 08:41:22 -0600

    proofread fixes

--- python-project/____tools____/pyskillz_tools.py
+++ python-project/____tools____/pyskillz_tools.py
@@ -738,6 +738,11 @@ class ExerciseTemplate():
             if '{or}' in line:
                 options = line.split(' {or} ')
                 text[i] = options[self.type == 'PrintBasedExercise']
+                
+            if 'return value or output' in line:
+                to_replace = 'return value or output'
+                options = line.split(' or ')
+                text[i] = text[i].replace(to_replace, options[self.type == 'PrintBasedExercise'])
 
         return text
     
